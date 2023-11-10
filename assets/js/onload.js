a49ba21444a38cbe84741863e29adef07e5d2cca
JingHai_Lingyun <m56042764@163.com>
2023-11-10 21:50:52 +0800

    :rocket: fix #1 加载动画

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -1,4 +1,13 @@
 window.onload = function() {
+    // 页面内容初始化函数
     editTitle()
     scrollToTop()
+    // 加载结束后等待 1~3s 设置加载动画不可见
+    setTimeout(() => {
+        document.getElementById('page-loading').style.opacity = 0
+        setTimeout(() => {
+            document.getElementById('page-loading').style.display = 'none'
+            document.body.style.overflow = 'auto';
+        }, 500)
+    }, Math.random() / 3 * 10 * 1000)
 }
