bedb9424ff5ae0995f85e2f6b49589c81e428960
JingHai_Lingyun <m56042764@163.com>
2023-11-12 12:02:32 +0800

    :bug: fix #4 调整时钟为向右计算偏移

--- assets/js/font.js
+++ assets/js/font.js
@@ -1,5 +1,5 @@
 function setFontPathWithEnv(){
-    if(document.referrer.includes("http://127.0.0.1")){
+    if(document.referrer.includes("http://127.0.0.1") || document.referrer == ''){
         setFontFace("千里之行楷","/assets/css/fonts/QianLiZhiXingKai-2.woff")
     } else {
         setFontFace("千里之行楷","https://lingyuncdn.rth1.app/QianLiZhiXingKai-2.woff")
